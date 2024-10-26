111ed3a3d285b55c187585a38a9c5d7c0ed368c5
Mike Ambrozaitis <10839603+mambroz27@users.noreply.github.com>
2024-10-26 08:40:30 -0400

    style: add prime config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeNGConfig } from 'primeng/api';
+import { Aura } from 'primeng/themes/aura';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  private primeConfig = inject(PrimeNGConfig);
+
+  constructor() {
+    this.primeConfig.theme.set({
+      preset: Aura,
+      options: {
+        darkModeSelector: '.dark',
+        cssLayer: {
+          order: 'tailwind-base, primeng, tailwind-utilities',
+        },
+      },
+    });
+  }
+
   title = 'PuzzleBreak';
 }
